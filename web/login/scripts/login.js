ce02d8bca0ac81b8934db33a3b152b3a41894413
JulianDomingo <julian.domingo@utexas.edu>
2017-11-18 17:56:46 -0600

    Added exhaustive skeleton list of possible errors for auth functions.

--- web/login/scripts/login.js
+++ web/login/scripts/login.js
@@ -1,17 +1,3 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
-
-// Sign in users.
-//firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    //// Handle errors here.
-    //var errorCode = error.code;
-    //console.log(error.Message);
-//});
-
-//// Triggers when the auth state change for instance when the user signs-in or signs-out.
-////FriendlyChat.prototype.onAuthStateChanged = function(user) {
-//firebase.auth().onAuthStateChanged = function(user) {
-    //console.log('onAuthStateChanged called.');
-    //window.location = (user) ? 'chat/chat.html' : 'index.html';
-//};
