84f5d488b5c5e183ced57a390e81a154e832643e
JulianDomingo <julian.domingo@utexas.edu>
2017-11-18 16:26:34 -0600

    Simplified listener for authentication status.

--- web/login/scripts/login.js
+++ web/login/scripts/login.js
@@ -12,17 +12,5 @@ firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 //FriendlyChat.prototype.onAuthStateChanged = function(user) {
 firebase.auth().onAuthStateChanged = function(user) {
-  if (user) { 
-    // User is signed in, redirect user to the chat page.
-    window.location = 'chat/chat.html';
-  } else { 
-    // User is signed out!
-    // Hide user's profile and sign-out button.
-    this.userName.setAttribute('hidden', 'true');
-    this.userPic.setAttribute('hidden', 'true');
-    this.signOutButton.setAttribute('hidden', 'true');
-
-    // Show sign-in button.
-    this.signInButton.removeAttribute('hidden');
-  }
+    window.location = (user) ? 'chat.chat.html' : 'index.html';
 };
