3affa4f3e085ea0179d50cbde364a9def9ceee6b
JulianDomingo <julian.domingo@utexas.edu>
2017-11-18 15:38:20 -0600

    Added WIP user auth with email/pass combo.

--- web/login/scripts/login.js
+++ web/login/scripts/login.js
@@ -1,3 +1,28 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+// Sign in users.
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle errors here.
+    var errorCode = error.code;
+    console.log(error.Message);
+});
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+//FriendlyChat.prototype.onAuthStateChanged = function(user) {
+firebase.auth().onAuthStateChanged = function(user) {
+  if (user) { 
+    // User is signed in, redirect user to the chat page.
+    window.location = 'chat.html';
+  } else { 
+    // User is signed out!
+    // Hide user's profile and sign-out button.
+    this.userName.setAttribute('hidden', 'true');
+    this.userPic.setAttribute('hidden', 'true');
+    this.signOutButton.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    this.signInButton.removeAttribute('hidden');
+  }
+};
