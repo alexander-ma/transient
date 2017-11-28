eb236ceb0239c75a47183a19941e141e2f549f5a
Jake DesJarlais <jake.desjarlais@yahoo.com>
2017-11-27 23:33:09 -0600

    Fixed dependancies issues with TransientCronjob
    	Added google auth http and credentials libraries

--- transient-cronjob/src/TransientCronjob.java
+++ transient-cronjob/src/TransientCronjob.java
@@ -1,17 +1,19 @@
-
 import java.util.Timer;
 import java.util.TimerTask;
 import java.io.FileInputStream;
 
+import com.google.auth.oauth2.GoogleCredentials;
+
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.FirebaseApp;
-
 
 
 public class TransientCronjob extends TimerTask {
   /* Settings */
   private static final String AUTHENTICATION_KEY_FILE =
     "resources/transient-318de-firebase-key.json";
+  private static final String DATABASE_URL =
+    "https://transient-318de.firebaseio.com";
   private static final boolean RUN_AS_DAEMON = true;
   private static final long DEFAULT_JOB_INTERVAL = 1000;
 
@@ -23,7 +25,7 @@ public class TransientCronjob extends TimerTask {
     FirebaseOptions options = null;
 
     try {
-      serviceAccount = new FileInputStream("resources/transient-318de-firebase-key.json");
+      serviceAccount = new FileInputStream(AUTHENTICATION_KEY_FILE);
     } catch (Exception e) {
       System.out.println("Error loading service account key file:");
       System.out.println(e.getMessage());
@@ -31,27 +33,28 @@ public class TransientCronjob extends TimerTask {
     }
 
     try {
-      options = new FirebaseOptions.Builder()
-        .setCredential(com.google.firebase.auth.FirebaseCredentials.fromCertificate(serviceAccount))
-        .setDatabaseUrl("https://transient-318de.firebaseio.com")
-        .build();
+        options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl(DATABASE_URL)
+            .build();
     } catch (Exception e) {
       System.out.println("Error authenticating key with firebase:");
       System.out.println(e.getMessage());
       System.exit(0);
     }
 
-      FirebaseApp.initializeApp(options);
+    FirebaseApp.initializeApp(options);
   }
 
 
   @Override
   public void run() {
+      // TODO:
   }
 
 
   public static void main(String args[]) {
-    TimerTask cronjob = new TransientCronjob();
+      TimerTask cronjob = new TransientCronjob();
     long interval = DEFAULT_JOB_INTERVAL;
 
     if (args.length >= 1) {
