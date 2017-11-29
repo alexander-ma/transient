36dbab2fb05b5ccd8253369411ed37c10b8a0a23
Julian Domingo <julian.domingo@utexas.edu>
2017-11-29 15:46:52 -0600

    Updated random name generator to act on users instead of channels

--- web/functions/index.js
+++ web/functions/index.js
@@ -2,18 +2,22 @@ const functions = require('firebase-functions');
 const sentencer = require('sentencer');
 
 /* 
- * Generates a random channel name in the form "<adjective> <noun>" for the
- * newly created channel. 
+ * Generates a random anonymous name in the form "<adjective> <noun>" for the
+ * newly created user. 
  */ 
-exports.generate = functions.database.ref('/users/{user-hash}/live-channels/{channel-hash}')
-    .onWrite(event => {
+exports.generateAnonymousName = functions.database.ref('/users/{user-hash}')
+    .onCreate(event => {
         // Guard database writes to prevent unintended recursive calls. 
         if (event.data.previous.exists() || !event.data.exists()) { return null; }
         
         var randomName = sentencer.make("{{ adjective }} {{ noun }}");
-        console.log("Generated channel name: " + randomName);
+        console.log("Generated user anonymous name: " + randomName);
         var adminRef = event.data.adminRef;
 
         // Update the placeholder channel name to the randomly generated one.
-        return adminRef.set(randomName);
+        //return adminRef.set({ anonymousName : randomName });
+        //return adminRef.push({ anonymousName : randomName });
+        return adminRef.update({anonymousName : randomName });
     });
+
+
